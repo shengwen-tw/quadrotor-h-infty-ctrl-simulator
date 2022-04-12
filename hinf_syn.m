59dfb90fc88df661b58526e7c9e815a6ee357cea
Shengwen Cheng <c.shengwen.tw@gmail.com>
2022-04-12 14:47:17 +0800

    optimize care_sda

--- hinf_syn.m
+++ hinf_syn.m
@@ -111,7 +111,7 @@ function retval=has_pure_img_eigen(H)
     [V, D] = eig(H);
         
     for i= 1 : max(size(H))
-        if(~isreal(D(i, i)) && abs(real(D(i, i))) < 1e-3)
+        if(~isreal(D(i, i)) && abs(real(D(i, i))) < 1e-2)
             retval = 1;
             return;
         end
@@ -141,21 +141,21 @@ iteration_times = 0;
 
 %solve CARE with SDA
 A_hat_last = I + 2*r*inv(A_r + G*transpose(inv(A_r))*H);
-G_hat_last = 2*r*inv(A_r)*G*inv(A_r.' + H*inv(A_r)*G);
-H_hat_last = 2*r*inv(A_r.' + H*inv(A_r)*G)*H*inv(A_r);
+G_hat_last = 2*r*(A_r\G/(A_r.' + H/A_r*G));
+H_hat_last = 2*r*inv(A_r.' + H/A_r*G)*H/A_r;
 norm_H_now = norm(H_hat_last);
 
 while 1
     iteration_times = iteration_times + 1;
     
     %reduce redundent calculation by pre-calculating repeated terms
-    inv_I_plus_H_G = inv(I + (H_hat_last * G_hat_last));
+    I_plus_H_G = I + (H_hat_last * G_hat_last);
     A_hat_last_t = A_hat_last.';
     
     %update
-    A_hat_new = A_hat_last * inv(I + G_hat_last * H_hat_last) * A_hat_last;
-    G_hat_new = G_hat_last + (A_hat_last * G_hat_last * inv_I_plus_H_G * A_hat_last_t);
-    H_Hat_new = H_hat_last + (A_hat_last_t * inv_I_plus_H_G * H_hat_last * A_hat_last);
+    A_hat_new = A_hat_last * ((I + G_hat_last * H_hat_last) \ A_hat_last);
+    G_hat_new = G_hat_last + (A_hat_last * G_hat_last * (I_plus_H_G \ A_hat_last_t));
+    H_Hat_new = H_hat_last + (A_hat_last_t * (I_plus_H_G \ H_hat_last * A_hat_last));
         
     %matrix norms
     norm_H_last = norm_H_now;
