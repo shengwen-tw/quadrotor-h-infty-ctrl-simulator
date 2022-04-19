8e504ea424a5ae32c56ac9ae1de877a599a71dbd
Shengwen Cheng <c.shengwen.tw@gmail.com>
2022-04-19 17:00:11 +0800

    improve numerical stability of sda

--- care_sda.m
+++ care_sda.m
@@ -1,32 +1,33 @@
-function X = care_sda(A, B, H, G)
+function [X, fail] = care_sda(A, H, G)
+fail = 0;
 state_dimension = length(A);
 
 r = 2.4; %SDA's author suggested the value between 2.1~2.6
 I = eye(state_dimension);
-A_trans = transpose(A);
 A_r = A - (r*I);
 
 iteration_times = 0;
 
 %solve CARE with SDA
-A_hat_last = I + 2*r*inv(A_r + G*inv(transpose(A_r))*H);
-G_hat_last = 2*r*inv(A_r)*G*inv(transpose(A_r) + H*inv(A_r)*G);
-H_hat_last = 2*r*inv(transpose(A_r) + H*inv(A_r)*G)*H*inv(A_r);
+A_hat_last = I + 2*r*inv(A_r + G*transpose(inv(A_r))*H);
+G_hat_last = 2*r*(A_r\G/(A_r.' + H/A_r*G));
+H_hat_last = 2*r*inv(A_r.' + H/A_r*G)*H/A_r;
+norm_H_now = norm(H_hat_last);
 
 while 1
     iteration_times = iteration_times + 1;
     
     %reduce redundent calculation by pre-calculating repeated terms
-    inv_I_plus_H_G = inv(I + (H_hat_last * G_hat_last));
-    transpose_A_hat_last = transpose(A_hat_last);
+    I_plus_H_G = I + (H_hat_last * G_hat_last);
+    A_hat_last_t = A_hat_last.';
     
     %update
-    A_hat_new = A_hat_last * inv(I + G_hat_last * H_hat_last) * A_hat_last;
-    G_hat_new = G_hat_last + (A_hat_last * G_hat_last * inv_I_plus_H_G * transpose_A_hat_last);
-    H_Hat_new = H_hat_last + (transpose_A_hat_last * inv_I_plus_H_G * H_hat_last * A_hat_last);
+    A_hat_new = A_hat_last * ((I + G_hat_last * H_hat_last) \ A_hat_last);
+    G_hat_new = G_hat_last + (A_hat_last * G_hat_last * (I_plus_H_G \ A_hat_last_t));
+    H_Hat_new = H_hat_last + (A_hat_last_t * (I_plus_H_G \ H_hat_last * A_hat_last));
     
     %matrix norms
-    norm_H_last = norm(H_hat_last);
+    norm_H_last = norm_H_now;
     norm_H_now = norm(H_Hat_new);
     
     %prepare next iteration
@@ -34,8 +35,15 @@ while 1
     G_hat_last = G_hat_new;
     H_hat_last = H_Hat_new;
     
+    %diverge
+    if (norm_H_now - norm_H_last) < 0
+        X = [];
+        fail = 1;
+        return;
+    end
+    
     %stop iteration if converged
-    if abs(norm_H_now - norm_H_last) < 0.000000001 %* abs(norm_H_now)
+    if abs(norm_H_now - norm_H_last) < 1e-16
         break;
     end
     
--- hinf_syn.m
+++ hinf_syn.m
@@ -8,7 +8,7 @@ At = A.';
 B2t = B2.';
 H = B2 * B2t;
 G = C1.' * C1;
-Z = care_sda(At, 0, H, G); %a fixed-point iteration algorithm for solving CARE
+[Z, fail] = care_sda(At, H, G); %a fixed-point iteration algorithm for solving CARE
 %z_norm = norm(A*Z + Z*At - Z*G*Z + H);
 %disp(z_norm)
 B1t = B1.';
@@ -41,12 +41,15 @@ while 1
          -C1t*C1,                   -A.'            ];
     
     if(has_pure_img_eigen(H) == 0)
+        %[T1, T2, fail] = msfa(H);
+        %X = T2 / T1;
+        
         H = C1.'*C1;
         G = B2*B2.' - (1 / (gamma*gamma) .* B1*B1.');
-        [X, sda_converged] = modified_sda(A, 0, H, G);
-        
+        [X, fail] = care_sda(A, H, G);
+                
         %check if the solution is stable
-        if(sda_converged == 1)
+        if(fail == 0)
             X_norm = norm(At*X + X*A - X*G*X + H);
             if(X_norm < care_norm_eps)
                 %found a good gamma and CARE solution
@@ -68,6 +71,11 @@ while 1
     %bisection value is converged
     if (gamma_u - gamma_l) < eps
         if(optimal_norm < care_norm_eps)
+            %H = [   A,     1/(gamma*gamma).*(B1*B1t) - B2*B2t;
+            %     -C1t*C1,                   -A.'            ];
+            %[T1, T2, fail] = msfa(H);
+            %X = T2 / T1;
+
             gamma = optimal_gamma;
             X = optimal_X;
             return;
@@ -77,7 +85,7 @@ while 1
             G = B2*B2.';
             X = care_sda(A, 0, H, G);
             X_norm = norm(At*X + X*A - X*G*X + H);
-            gamma = 0; %just convenient for plotting
+            %gamma = 0; %just convenient for plotting
             return;
         end
     end
@@ -137,59 +145,4 @@ function flag=is_psd_matrix(T1, T2)
             return;
         end
     end
-end
-
-function [X, state] = modified_sda(A, B, H, G)
-state = 1;
-state_dimension = length(A);
-
-r = 2.4; %SDA's author suggested the value between 2.1~2.6
-I = eye(state_dimension);
-A_r = A - (r*I);
-
-iteration_times = 0;
-
-%solve CARE with SDA
-A_hat_last = I + 2*r*inv(A_r + G*transpose(inv(A_r))*H);
-G_hat_last = 2*r*(A_r\G/(A_r.' + H/A_r*G));
-H_hat_last = 2*r*inv(A_r.' + H/A_r*G)*H/A_r;
-norm_H_now = norm(H_hat_last);
-
-while 1
-    iteration_times = iteration_times + 1;
-    
-    %reduce redundent calculation by pre-calculating repeated terms
-    I_plus_H_G = I + (H_hat_last * G_hat_last);
-    A_hat_last_t = A_hat_last.';
-    
-    %update
-    A_hat_new = A_hat_last * ((I + G_hat_last * H_hat_last) \ A_hat_last);
-    G_hat_new = G_hat_last + (A_hat_last * G_hat_last * (I_plus_H_G \ A_hat_last_t));
-    H_Hat_new = H_hat_last + (A_hat_last_t * (I_plus_H_G \ H_hat_last * A_hat_last));
-    
-    %matrix norms
-    norm_H_last = norm_H_now;
-    norm_H_now = norm(H_Hat_new);
-    
-    %prepare next iteration
-    A_hat_last = A_hat_new;
-    G_hat_last = G_hat_new;
-    H_hat_last = H_Hat_new;
-    
-    %diverge
-    if (norm_H_now - norm_H_last) < 0
-        X = [];
-        state = 0;
-        return;
-    end
-    
-    %stop iteration if converged
-    if abs(norm_H_now - norm_H_last) < 1e-16
-        break;
-    end
-    
-    %disp(iteration_times);
-end
-
-X = H_Hat_new;
 end
