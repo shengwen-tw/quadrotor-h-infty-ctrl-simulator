5f102b88871b9e239b6e2dbf64df54026b1abb0b
Shengwen Cheng <c.shengwen.tw@gmail.com>
2022-04-19 23:10:59 +0800

    rearrange the bisection method code

--- hinf_syn.m
+++ hinf_syn.m
@@ -1,19 +1,24 @@
-function [gamma, X, X_norm]=hinf_syn(A, B1, B2, C1, D)
+%a bisection method to sythesis the H-infinity control solution with
+%minimal gamma using the structure-preserving doubling algorithm
+function [gamma, X, ric_residual]=hinf_syn(A, B1, B2, C1, D)
 eps = 1e-6;
-care_norm_eps = 1e-4;
+residual_eps = 1e-4;
+
+%record of total iteration numbers
 iteration = 0;
 
-%calculate lower bound gamma
+%common variables
 At = A.';
-B2t = B2.';
-H = B2 * B2t;
-G = C1.' * C1;
-[Z, fail] = care_sda(At, H, G); %a fixed-point iteration algorithm for solving CARE
-%z_norm = norm(A*Z + Z*At - Z*G*Z + H);
-%disp(z_norm)
-B1t = B1.';
-C1t = C1.';
-gamma_l = hinf_norm((A - Z*C1t*C1).', C1t, B1t, 0);
+B1B1t = B1*B1.';
+B2B2t = B2*B2.';
+C1tC1 = C1.'*C1;
+
+%calculate lower bound gamma
+H = B2B2t;
+G = C1tC1;
+[Z, fail] = care_sda(At, H, G);
+gamma_l = hinf_norm((A - Z*C1tC1).', C1.', B1.', 0);
+%disp(norm(A*Z + Z*At - Z*G*Z + H));
 %disp('gamma_lb:');
 %disp(gamma_l);
 
@@ -26,92 +31,65 @@ gamma_u = rho_max / rho_0;
 %disp('gamma_ub');
 %disp(gamma_u);
 
-optimal_norm = 1e10;
+optimal_residual = 1e10;
 optimal_gamma = gamma_u;
 optimal_X = [];
 
-%bisection method for optimal gamma and CARE solution
+%bisection searching for optimal gamma and CARE solution
 while 1
     iteration = iteration + 1;
     
     gamma = (gamma_l + gamma_u) / 2;
     
-    %construct Hamiltonian matrix
-    H = [   A,     1/(gamma*gamma).*(B1*B1t) - B2*B2t;
-         -C1t*C1,                   -A.'            ];
+    H = C1tC1;
+    G = B2B2t - (1 / (gamma*gamma) .* B1B1t);
     
-    if(has_pure_img_eigen(H) == 0)
-        %[T1, T2, fail] = msfa(H);
-        %X = T2 / T1;
-        
-        H = C1.'*C1;
-        G = B2*B2.' - (1 / (gamma*gamma) .* B1*B1.');
+    %construct Hamiltonian matrix
+    Ham = [ A, -G;
+           -H, -At];
+    
+    if(has_pure_img_eigen(Ham) == 0)
         [X, fail] = care_sda(A, H, G);
-                
-        %check if the solution is stable
+        
+        %if SDA manage to produce a solution
         if(fail == 0)
-            X_norm = norm(At*X + X*A - X*G*X + H);
-            if(X_norm < care_norm_eps)
+            %check if the solution is stable
+            ric_residual = norm(At*X + X*A - X*G*X + H);
+            if(ric_residual < residual_eps)
                 %found a good gamma and CARE solution
-                optimal_norm = X_norm;
+                optimal_residual = ric_residual;
                 optimal_gamma = gamma;
                 optimal_X = X;
                 %
-                gamma_u = gamma;
+                gamma_u = gamma; %decrease the upper bound
             else
-                gamma_l = gamma;
+                gamma_l = gamma; %increase the lower bound
             end
         else
-            gamma_l = gamma;
+            gamma_l = gamma; %increase the lower bound
         end
     else
-        gamma_l = gamma;
+        gamma_l = gamma; %increase the lower bound
     end
     
-    %bisection value is converged
+    %bisection searching is converged
     if (gamma_u - gamma_l) < eps
-        if(optimal_norm < care_norm_eps)
-            %H = [   A,     1/(gamma*gamma).*(B1*B1t) - B2*B2t;
-            %     -C1t*C1,                   -A.'            ];
-            %[T1, T2, fail] = msfa(H);
-            %X = T2 / T1;
-
+        if(optimal_residual < residual_eps)
+            %accept the solution
             gamma = optimal_gamma;
             X = optimal_X;
+            %disp(iteration);
             return;
         else
             %no solution found, return H2 control solution
-            H = C1.'*C1;
-            G = B2*B2.';
-            X = care_sda(A, 0, H, G);
-            X_norm = norm(At*X + X*A - X*G*X + H);
-            %gamma = 0; %just convenient for plotting
+            G = B2B2t;
+            X = care_sda(A, H, G);
+            ric_residual = norm(At*X + X*A - X*G*X + H);
+            gamma = 0; %just for convenient
             return;
         end
     end
 end
-end
-
-function [T1, T2]=get_stable_invariant_subspace(H)
-[V, D] = eig(H);
-[m, n] = size(H);
-
-V_stable = [];
-D_stable = zeros(m/2, n/2);
-j = 1;
-for i = 1:m
-    if(real(D(i, i)) < -1e-6)
-        V_stable = [V_stable, V(:, i)];
-        D_stable(j, j) = D(i, i);
-        j = j + 1;
-    end
-end
-
-T1 = V_stable(1:m/2, :);
-T2 = V_stable(m/2+1:m, :);
-
-%disp(diag(D_stable))
-%disp(norm(H*V_new - V_new*D_new));
 end
 
 function retval=has_pure_img_eigen(H)
@@ -124,25 +102,4 @@ for i= 1 : max(size(H))
         return;
     end
 end
-end
-
-%check "calculating the inertia of a real symmetric (or tridiagonal) matrix"
-%on "math.stackexchange.com"
-function flag=is_psd_matrix(T1, T2)
-    flag = 1;
-    [P, Omega] = tridiag(T1.'*T2);
-    [L, D] = ldl_tri(Omega);
-    
-    %disp(Omega);
-    %disp(D);
-    
-    %check inertia of the matrix
-    for i = 1:max(size(Omega))
-        %if exisits negative element of the D matrix,
-        %X is not P.S.D
-        if(D(i, i) < 0)
-            flag = 0;
-            return;
-        end
-    end
 end
--- msfa.m
+++ msfa.m
@@ -1,20 +1,51 @@
+%matrix sign function algorithm
 function [T1, T2, fail]=msfa(H)
 n = max(size(H));
 I = eye(n);
-%H = H - 0.8*sqrt(-1)*I;
+%rcond(H)
 %disp(min(eig(H)));
-H_last = balance(H);
+H_last = H;
 H_now = [];
+
+%solve sign(H)
 while 1
-    H_now = (H_last + inv(H_last)) / 2;
+    %[U, S, V] = svd(H_last);
+    %inv_H_last = V / S * U';
+
+    inv_H_last = inv(H_last);
+    %disp(norm(H_last * inv_H_last))
+    
+    H_now = (H_last + inv_H_last) / 2;
     residual = norm(H_now - H_last);
     H_last = H_now;
-    if(residual < 1e-10)
+    if residual < 1e-6
         break;
     end
 end
-[Q, R] = qr(H_now - I);
+
+Q = null(I - H_now); %solve (I - sign(H)) * X = 0
+%[Q, R] = qr(H_now - I);
+
 T1 = Q(1:n/2, 1:n/2);
 T2 = Q(n/2+1:n, 1:n/2);
+
 fail = 0;
+end
+
+%check "calculating the inertia of a real symmetric (or tridiagonal) matrix"
+%on "math.stackexchange.com"
+function flag=is_psd_matrix(T1, T2)
+    flag = 1;
+    %[P, Omega] = tridiag(T1.'*T2);
+    %[L, D] = ldl_tri(Omega);
+    %disp(Omega);
+    %disp(D);
+    [V, D] = eig(T1.'*T2);
+    
+    for i = 1:max(size(Omega))
+        if(D(i, i) < 0)
+            flag = 0;
+            return;
+        end
+    end
 end
